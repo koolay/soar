6207c121950324909048e92e1def61b2b72b03e4
koolay <koolay@163.com>
2020-08-24 16:37:00 +0800

    fix main

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	sql := "select t1.* from tab t1 inner join tb2 as t2 on t1.id = t2.fid"
-	output, err := run.Run(sql, 7)
+	output, err := run.Run("mydb", sql, 7)
 	if err != nil {
 		panic(err)
 	}
